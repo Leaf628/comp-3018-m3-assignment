c6979d5866d085e4fc077bc443034a7ee8804083
Kang Ye <kye2@academic.rrc.ca>
2026-02-13 16:34:46 -0600

    Set up the structure by modifying package.json, app.ts and package-lock.json.

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Express } from "express";
+import morgan from "morgan";
+import router from "../src/api/v1/routes/postRoutes"
+
+const app: Express = express();
+
+app.use(express.json()); //  use JSON body parsing
+
+// Use Morgan for HTTP request logging
+app.use(morgan("combined"));
+
+// GET request at the app root
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
